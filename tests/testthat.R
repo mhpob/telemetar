c3a0d6243355b818ef342800b1abf98570e539b4
Mike O'Brien <mikeob9@gmail.com>
2024-02-02 19:39:24 -0500

    start fleshing out tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,7 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
+library(targets)
 library(telemetar)
 
 test_check("telemetar")
--- tests/testthat/test-tar_vue_csvs.R
+++ tests/testthat/test-tar_vue_csvs.R
@@ -1,17 +1,71 @@
-fls <- list.files('c:/users/darpa2/analysis/chesapeake-backbone/embargo/raw/20230912',
-           pattern = "^VR.*\\.vrl$", full.names = T)[1:2]
+# tar_test("batching works", {
+#
+# })
 
+targets::tar_test("all files in one dir (to rule them all)", {
+  ## Download example data
+  dir.create('td', recursive = TRUE)
+  download.file(
+    file.path('https://raw.githubusercontent.com/ocean-tracking-network/glatos',
+              'main/inst/extdata/VR2W_109924_20110718_1.csv'),
+    'td/VR2W_109924_20110718_1.csv'
+  )
+  for(i in 2:12){
+    file.copy(
+      'td/VR2W_109924_20110718_1.csv',
+      paste0('td/VR2W_109924_20110718_', i, '.csv')
+    )
+  }
 
+  ## Run workflow
   targets::tar_script(
     list(
       telemetar::tar_vue_csvs(
-        'c:/users/darpa2/analysis/chesapeake-backbone/embargo/raw/20230912'
+        my_detections,
+        'td'
       )
     )
   )
   targets::tar_make()
 
+  ## Start tests
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  manifest <- tar_manifest()
+})
+
+targets::tar_test("files in separate dirs (in the darkness bind them)", {
+  ## Download example data
+  dir.create('td/td2', recursive = TRUE)
+  download.file(
+    file.path('https://raw.githubusercontent.com/ocean-tracking-network/glatos',
+              'main/inst/extdata/VR2W_109924_20110718_1.csv'),
+    'td/VR2W_109924_20110718_1.csv'
+  )
+  for(i in 2:5){
+    file.copy(
+      'td/VR2W_109924_20110718_1.csv',
+      paste0('td/VR2W_109924_20110718_', i, '.csv')
+    )
+    file.copy(
+      'td/VR2W_109924_20110718_1.csv',
+      paste0('td/td2/VR2W_109924_20110718_', i, '.csv')
+    )
+  }
+
+  ## Run workflow
+  targets::tar_script(
+    list(
+      telemetar::tar_vue_csvs(
+        my_detections,
+        'td'
+      )
+    )
+  )
+  targets::tar_make()
+
+  ## Start tests
+  out <- tar_manifest(my_detections)
+
+  expect_equal(out$name, "my_detections")
+  expect_equal(out$pattern, "map(my_detections_tracked)")
 })
