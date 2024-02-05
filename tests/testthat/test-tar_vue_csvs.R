90a4ed6f857ab8a2ee24e400a6f43f96514a89e1
Mike O'Brien <mikeob9@gmail.com>
2024-02-05 10:22:17 -0500

    more tar_vue_csvs tests

--- tests/testthat/test-tar_vue_csvs.R
+++ tests/testthat/test-tar_vue_csvs.R
@@ -1,10 +1,6 @@
-# tar_test("batching works", {
-#
-# })
-
 targets::tar_test("all files in one dir (to rule them all)", {
   ## Download example data
-  dir.create('td', recursive = TRUE)
+  dir.create('td')
   download.file(
     file.path('https://raw.githubusercontent.com/ocean-tracking-network/glatos',
               'main/inst/extdata/VR2W_109924_20110718_1.csv'),
@@ -26,16 +22,45 @@ targets::tar_test("all files in one dir (to rule them all)", {
       )
     )
   )
-  targets::tar_make()
+  targets::tar_make(callr_function = NULL)
 
   ## Start tests
 
-  manifest <- tar_manifest()
+  out <- targets::tar_manifest()
+
+  expect_equal(out$name, "my_detections")
+  expect_equal(out$pattern, "map(my_detections_tracked)")
+  expect_true(grepl('csv_dir', out$command))
+
+  # Branching works
+  expect_false(any(is.na(tar_branch_names(my_detections, 1:2))))
+  expect_true(is.na(tar_branch_names(my_detections, 3)))
+
+  # Correct types
+  md <- targets::tar_load(my_detections)
+  expect_s3_class(md, c('data.table', 'data.frame'), exact = TRUE)
+  expect_named(md, c('datetime', 'receiver', 'transmitter', 'transmittername',
+                     'transmitterserial', 'sensorvalue', 'sensorunit',
+                     'stationname', 'latitude', 'longitude'))
+
+  md_files <- targets::tar_read(my_detections_tracked)
+  expect_length(md_files, 3)
+  expect_type(md_files, 'character')
+
+  md_files_list <- targets::tar_read(my_detections_tracked_files)
+  expect_length(md_files_list, 3)
+  expect_type(md_files_list, 'list')
+
+
+  # unlink('td', recursive = TRUE) # only needed for interactive testing
 })
+
+
+
 
 targets::tar_test("files in separate dirs (in the darkness bind them)", {
   ## Download example data
-  dir.create('td/td2', recursive = TRUE)
+  dir.create('td/td2/td3', recursive = TRUE)
   download.file(
     file.path('https://raw.githubusercontent.com/ocean-tracking-network/glatos',
               'main/inst/extdata/VR2W_109924_20110718_1.csv'),
@@ -50,6 +75,10 @@ targets::tar_test("files in separate dirs (in the darkness bind them)", {
       'td/VR2W_109924_20110718_1.csv',
       paste0('td/td2/VR2W_109924_20110718_', i, '.csv')
     )
+    file.copy(
+      'td/VR2W_109924_20110718_1.csv',
+      paste0('td/td2/td3/VR2W_109924_20110718_', i, '.csv')
+    )
   }
 
   ## Run workflow
@@ -61,11 +90,34 @@ targets::tar_test("files in separate dirs (in the darkness bind them)", {
       )
     )
   )
-  targets::tar_make()
+  targets::tar_make(callr_function = NULL)
 
   ## Start tests
-  out <- tar_manifest(my_detections)
+  out <- targets::tar_manifest(my_detections)
 
   expect_equal(out$name, "my_detections")
   expect_equal(out$pattern, "map(my_detections_tracked)")
+  expect_true(grepl('csv_dir', out$command))
+
+  # Branching works
+  expect_false(any(is.na(tar_branch_names(my_detections, 1:3))))
+  expect_true(is.na(tar_branch_names(my_detections, 4)))
+
+  # Correct types
+  md <- targets::tar_load(my_detections)
+  expect_s3_class(md, c('data.table', 'data.frame'), exact = TRUE)
+  expect_named(md, c('datetime', 'receiver', 'transmitter', 'transmittername',
+                      'transmitterserial', 'sensorvalue', 'sensorunit',
+                      'stationname', 'latitude', 'longitude'))
+
+  md_files <- targets::tar_read(my_detections_tracked)
+  expect_length(md_files, 3)
+  expect_type(md_files, 'character')
+
+  md_files_list <- targets::tar_read(my_detections_tracked_files)
+  expect_length(md_files_list, 3)
+  expect_type(md_files_list, 'list')
+
+
+  # unlink('td', recursive = TRUE) # only needed for interactive testing
 })
