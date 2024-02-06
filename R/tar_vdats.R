8ca0e31655736dc7d23a7e527b5d7e03e5a0c867
Mike O'Brien <mikeob9@gmail.com>
2024-02-06 16:13:26 -0500

    draft vdat_read_in and document

--- R/tar_vdats.R
+++ R/tar_vdats.R
@@ -50,6 +50,7 @@ tar_vdat_read <- function(
       batches = batches
     )
 
+
   ## Convert
   sym_files <- as.symbol(name_files)
   name_csv <- paste0(name, '_csv')
@@ -70,6 +71,7 @@ tar_vdat_read <- function(
       format = 'qs'
     )
 
+
   ## Read
   sym_csv <- as.symbol(name_csv)
 
@@ -87,14 +89,20 @@ tar_vdat_read <- function(
       format = 'qs'
     )
 
+
   # Export target factory
   list(track_vdat, convert_vdat, read_vdat)
 }
 
 
 
-
-#' @export
+#' Convert VDAT to CSV and track
+#' @param vdat_batch a batch of VDAT file paths
+#' @param csv_outdir file path to the output directory
+#'
+#' @returns character vector of directory file paths holding VDAT-exported CSVs
+#'
+#' @keywords internal vdat
 tar_vdat_dir <- function(vdat_batch, csv_outdir){
   # Read files into the elements of a list
   for(i in seq_along(vdat_batch)){
@@ -105,35 +113,67 @@ tar_vdat_dir <- function(vdat_batch, csv_outdir){
     )
   }
 
-  list.files(csv_outdir)
+  list.dirs(csv_outdir)
 
 }
 
 
-#Notes to self:
-# look at csv_read_in and adapt
-vdat_read_in <- function(vrl_dir){
-  vrls <- list.files(vrl_dir, full.names = T,
-                     pattern = '^[VH]R.{2,3}_.*(\\.vrl|\\.vdat)')
+#' Read VDAT data into a merged list by data type
+#' @param vdat_dir directory of VDAT-exported CSVs
+#'
+#' @returns list of data types merged across receiver VDAT files in `vdat_dir`
+#'
+#' @keywords internal vdat
+vdat_read_in <- function(vdat_dir){
+  # Read files into the elements of a list
+  data_by_receiver <- lapply(
+    vdat_dir,
+    FUN = function(x){
+      vdat_csvs <- list.files(x, pattern = "\\.csv$", full.names = TRUE)
+      data_csv <- lapply(
+        vdat_csvs,
+        data.table::fread,
+        sep = ",",
+        fill = T,
+        skip = 2,
+        # rename columns
+        col.names = function(x){
+          tolower(gsub('and|UTC|[) (\\.]', '', x))
+        },
+        # some columns have no info, which causes class clashes in
+        #   data.table::rbindlist. Import all as character to skirt around this
+        colClasses = 'character'
+      )
+      names(data_csv) <- gsub("\\.csv$", "", basename(vdat_csvs))
 
-  if(length(csv_dir) == 0){
-    data.table::data.table()
-  } else {
-    # Read files into the elements of a list
-    detections <- lapply(
-      detections,
-      data.table::fread,
-      fill = T,
-      # rename columns
-      col.names = function(x){
-        tolower(gsub('and|UTC|[) (\\.]', '', x))
-      }
-    )
+      data_csv
+    }
+  )
 
+  # Select only this files with something in them
+  data_by_receiver <- data_by_receiver[sapply(data_by_receiver, length) > 0]
 
-    # Bind list together into a data.table
-    detections <- data.table::rbindlist(detections)
+  # Pull out the names of the different data types
+  data_names <- sapply(data_by_receiver, names) |> unlist() |> unique()
 
-    detections
-  }
+  # Merge the different data types together
+   data_by_type <- lapply(
+    data_names,
+    FUN = function(x){
+      hold <- lapply(data_by_receiver, `[[`, x) |>
+        data.table::rbindlist(use.names = TRUE, fill = TRUE)
+
+      # Fastest thing is to write and re-import to guess classes
+      #  utils::type.convert doesn't guess times correctly
+      data.table::fwrite(hold, file.path(tempdir(), 'hold.csv'))
+      hold <- data.table::fread(file.path(tempdir(), 'hold.csv'))
+    })
+
+  # clean up files
+  unlink(file.path(tempdir(), 'hold.csv'))
+
+  names(data_by_type) <- data_names
+
+  data_by_type
 }
+
